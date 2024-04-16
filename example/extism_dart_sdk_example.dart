3b47ee4b665b1bafbb3a9c1700a131f478f68871
Krysl <krysl@qq.com>
2024-04-16 20:43:00 +0800

    feat: 🎉 init with `dart create -t package`
    Signed-off-by: Krysl <krysl@qq.com>

---
+++ example/extism_dart_sdk_example.dart
@@ -0,0 +1,6 @@
+import 'package:extism_dart_sdk/extism_dart_sdk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/extism_dart_sdk_test.dart
@@ -0,0 +1,16 @@
+import 'package:extism_dart_sdk/extism_dart_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
